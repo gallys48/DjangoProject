fccd50031adfbb6129bb2bb5a0755b6ff75f0245
gallys <vitalya.grachev.79@mail.ru>
2022-10-16 19:01:13 +0700

    before adding models1

--- mysite/travels/models.py
+++ mysite/travels/models.py
@@ -1,3 +1,11 @@
+from email.policy import default
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Travel(models.Model):
+    title=models.CharField(max_lenght=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
