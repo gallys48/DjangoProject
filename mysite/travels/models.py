28c972a4f0a8731c33b176e5d9e9436effe866ab
gallys <vitalya.grachev.79@mail.ru>
2022-10-17 20:09:52 +0700

    add model Category

--- mysite/travels/models.py
+++ mysite/travels/models.py
@@ -16,10 +16,16 @@ class Travel(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
         return reverse("travel", kwargs={"travel_id": self.pk})
-    
+
+class Category(models.Model):
+    title=models.CharField(max_length=100 , db_index=True) 
+
+    def __str__(self):
+        return self.title
