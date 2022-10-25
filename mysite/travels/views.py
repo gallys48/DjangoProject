0c2bcf51d07655a0c0c968052b2d75854653d0f1
gallys <vitalya.grachev.79@mail.ru>
2022-10-25 12:20:26 +0700

    add .

--- mysite/travels/views.py
+++ mysite/travels/views.py
@@ -139,7 +139,7 @@ class RegisterUser(DataMixin, CreateView):
 class LoginUser(DataMixin, LoginView):
     form_class = LoginUserForm
     template_name = 'travels/login.html'
-    success_url = reverse_lazy('travels')
+    success_url = reverse_lazy('usertravels')
     def get_context_data(self, *,object_list=None,**kwargs):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Авторизация')
